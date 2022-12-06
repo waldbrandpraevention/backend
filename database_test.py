624fe4e4ebb9792c7a288f05fd9afd8667fcd25d
NSG22 <noelgoldschmidt@web.de>
2022-12-06 02:12:48 +0100

    basic func test

--- database_test.py
+++ database_test.py
@@ -1,27 +1,35 @@
 
 from classes import User, UserWithSensitiveInfo
 from database.database import create_table
-from database.users_table import create_user,get_user
+from database.mail_verif_table import check_token, get_mail_by_token, get_token_by_mail, store_token, CREATE_MAIL_VERIFY_TABLE
+from database.users_table import create_user,get_user,CREATE_USER_TABLE
+import time 
 
-CREATE_USER_TABLE = """ CREATE TABLE IF NOT EXISTS users (
-                        id INTEGER PRIMARY KEY,
-                        email TEXT NOT NULL UNIQUE,
-                        first_name TEXT NOT NULL,
-                        last_name TEXT NOT NULL,
-                        hashed_password TEXT NOT NULL,
-                        permission INTEGER DEFAULT 0,
-                        disabled INTEGER,
-                        email_verified INTEGER NOT NULL
-                    );"""
+mail = 'test@mail.de'
 
-create_table(CREATE_USER_TABLE)
-user = UserWithSensitiveInfo(email='test@mail.de',
-            first_name='Hans',
-            last_name='Dieter',
-            hashed_password='dsfsdfdsfsfddfsfd',
-            permission=1,
-            disabled=0,
-            email_verified=0)
-create_user(user)
-user = get_user('test@mail.de')
-print(user)
+def test_usertable():
+    create_table(CREATE_USER_TABLE)
+    user = UserWithSensitiveInfo(email=mail,
+                first_name='Hans',
+                last_name='Dieter',
+                hashed_password='dsfsdfdsfsfddfsfd',
+                permission=1,
+                disabled=0,
+                email_verified=0)
+    create_user(user)
+    user = get_user(mail)
+    #print(user)
+
+def test_verifytable():
+    create_table(CREATE_MAIL_VERIFY_TABLE)
+    token = 'ichbineintoken.:)'
+    store_token(mail,token)
+    print(check_token(token))
+    print(get_mail_by_token(token))
+    print(get_token_by_mail(mail))
+
+start = time.time()
+[test_usertable() for _ in range(500)]
+test_verifytable()
+end = time.time()
+print(end - start)
