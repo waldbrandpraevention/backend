bce810ad7eb3aa826120d0f0492b17ec5fad4a0e
NSG22 <noelgoldschmidt@web.de>
2022-12-12 15:11:11 +0100

    cleanup

--- tests/database_test.py
+++ tests/database_test.py
@@ -1,13 +1,16 @@
 
-from classes import User, UserWithSensitiveInfo
+from api.dependencies.classes import User, UserWithSensitiveInfo
 from database.database import create_table
 from database.mail_verif_table import check_token, get_mail_by_token, get_token_by_mail, store_token, CREATE_MAIL_VERIFY_TABLE
 from database.users_table import create_user,get_user,CREATE_USER_TABLE
+from database.organizations import CREATE_ORGANISATIONS_TABLE, create_orga
 import time 
 
 mail = 'test@mail.de'
 
 def test_usertable():
+    create_table(CREATE_ORGANISATIONS_TABLE)
+    create_orga('testorga','TO')
     create_table(CREATE_USER_TABLE)
     user = UserWithSensitiveInfo(email=mail,
                 first_name='Hans',
@@ -15,23 +18,24 @@ def test_usertable():
                 hashed_password='dsfsdfdsfsfddfsfd',
                 permission=1,
                 disabled=0,
-                email_verified=0)
+                email_verified=0,
+                organization = 1)
     create_user(user)
     user = get_user(mail)
     #print(user)
 
 def test_verifytable():
     create_table(CREATE_MAIL_VERIFY_TABLE)
-    token = 'ichbineintoken.:)'
+    token = 'ichbineintoken:)'
     store_token(mail,token)
-    store_token(mail,token, update=True)
+    store_token(mail,token)
     print(check_token(token))
     print(get_mail_by_token(token))
     print(get_token_by_mail(mail))
     
 
 start = time.time()
-[test_usertable() for _ in range(500)]
+test_usertable() #for _ in range(500)]
 test_verifytable()
 end = time.time()
 print(end - start)
