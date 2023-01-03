0ac42a780f9d64323ce18993a472f4a2d6e64929
NSG22 <noelgoldschmidt@web.de>
2023-01-04 00:31:09 +0100

    added comments

--- database/drone_data.py
+++ database/drone_data.py
@@ -2,6 +2,7 @@ import datetime
 from enum import Enum
 import json
 import sqlite3
+from typing import List
 from api.dependencies.classes import DroneData
 from database.database import database_connection, fetched_match_class
 
@@ -24,7 +25,7 @@ CREATE INDEX drone_data_FK_1 ON drone_data (drone_id);'''
 # FOREIGN KEY (zone_id) REFERENCES Zones (id),
 # CREATE INDEX drone_data_FK_1 ON drone_data (zone_id);
 CREATE_ENTRY = 'INSERT INTO drone_data (drone_id,timestamp,longitude,latitude,picture_path,ai_predictions,csv_file_path) VALUES (? ,?,?,? ,? ,?,?);'
-GET_ENTRYS_BY_TIMESTAMP = 'SELECT * FROM drone_data WHERE drone_id = ? AND timestamp > ?;'
+GET_ENTRYS_BY_TIMESTAMP = 'SELECT * FROM drone_data WHERE drone_id = ? AND timestamp > ? AND timestamp < ?;'
 
 
 def create_drone_zone_entry(drone_id:int,
@@ -33,25 +34,48 @@ def create_drone_zone_entry(drone_id:int,
                             latitude:float,
                             picture_path:str|None,
                             ai_predictions:dict|None,
-                            csv_file_path:str|None):
-    
+                            csv_file_path:str|None)-> bool:
+    """store the data sent by the drone.
+
+    Args:
+        drone_id (int): _description_
+        timestamp (datetime.datetime): _description_
+        longitude (float): _description_
+        latitude (float): _description_
+        picture_path (str | None): _description_
+        ai_predictions (dict | None): _description_
+        csv_file_path (str | None): _description_
+
+    Returns:
+        bool: True for success, False if something went wrong.
+    """
     try:
         with database_connection() as conn:
             cursor = conn.cursor()
             cursor.execute(CREATE_ENTRY,(drone_id,timestamp,str(longitude),str(latitude),picture_path,json.dumps(ai_predictions),csv_file_path))
             conn.commit()
             cursor.close()
-        
+            return True
     except sqlite3.IntegrityError as e:##TODO
         print(e)
-    return None
+    return False
 
-def get_drone_data_by_timestamp(drone_id:int,since_timestamp:datetime.datetime):
+def get_drone_data_by_timestamp(drone_id:int,after:datetime.datetime=datetime.datetime.min,before:datetime.datetime=datetime.datetime.max) -> List[DroneData]:
+    """fetches all entrys that are within the choosen timeframe.
+    If only drone_id is set, every entry will be fetched.
 
+    Args:
+        drone_id (int): _description_
+        after (datetime.datetime): fetches everything after this date (not included)
+        before (datetime.datetime): fetches everything before this date (not included)
+
+    Returns:
+        List[DroneData]: List with the fetched data.
+    """
     try:
         with database_connection() as conn:
             cursor = conn.cursor()
-            cursor.execute(GET_ENTRYS_BY_TIMESTAMP,(drone_id,since_timestamp))
+            cursor.execute(GET_ENTRYS_BY_TIMESTAMP,(drone_id,after,before))
             fetched_data = cursor.fetchall()
             cursor.close()
             output = []
@@ -64,8 +88,15 @@ def get_drone_data_by_timestamp(drone_id:int,since_timestamp:datetime.datetime):
         print(e)
 
 
-def get_obj_from_fetched(fetched_dronedata):
+def get_obj_from_fetched(fetched_dronedata) -> DroneData| None:
+    """generating DroneData objects with the fetched data.
 
+    Args:
+        fetched_dronedata: the fetched data from the sqlite cursor.
+
+    Returns:
+        DroneData| None: the generated object.
+    """
     if fetched_match_class(DroneData,fetched_dronedata):
         try:
             ai_predictions = json.loads(fetched_dronedata[5])
