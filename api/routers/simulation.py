ae94bf963de7a1abcab8fd055191233f9dc70014
Philipp <philipp.brodersen@web.de>
2023-02-02 03:08:48 +0100

    assets + some drone stuff

---
+++ api/routers/simulation.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+
+router = APIRouter()
+
+
+@router.get("/simulation/all-drones/", status_code=status.HTTP_200_OK, response_model=list[Zone])
+async def read_zones_all():
+    """API call to get all simulation drones.
+
+    Returns:
+        DronesForSimulation[]: List of drones.
+    """
+
+    #load the drones
+    return get_current_wind()
