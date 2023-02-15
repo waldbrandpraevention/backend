f4a2fa7cfd9cee27d5cf69e83ebce5544e420f38
Philipp <philipp.brodersen@web.de>
2023-02-15 17:49:09 +0100

    bug fix

--- api/routers/simulation.py
+++ api/routers/simulation.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from ..dependencies.classes import DroneForSimulation
 from ..dependencies.drones import generate_drone_token
 from database import drones_table, zones_table
+import math
 
 router = APIRouter()
 
@@ -22,16 +23,16 @@ async def get_sim_drones():
         zones = zones_table.get_zone_of_by_district(os.getenv("DEMO_DISTRICT"))
         
         for d in drones:
-            random = randrange(0, len(zones) - 1)
+            rand = randrange(0, len(zones) - 1)
             angle = random.random()
             d_sim = DroneForSimulation( 
                 drone = d,     
                 token = generate_drone_token(d.id),
-                geo_json = zone[random].geo_json,
+                geo_json = zone[rand].geo_json,
                 speed = 0.0001,
-                direction = (cos(angle), sin(angle)),
-                lat = zones[random].lat,
-                lon = zones[random].lan
+                direction = (math.cos(angle), math.sin(angle)),
+                lat = zones[rand].lat,
+                lon = zones[rand].lan
             )
             sim_drones.append(d_sim)
     except Exception as e:
