2116c838c44b65ccd40c2bacff8d83c0f31b2839
Philipp <philipp.brodersen@web.de>
2023-02-15 18:03:39 +0100

    bug fix

--- api/routers/emails.py
+++ api/routers/emails.py
@@ -6,7 +6,7 @@ from fastapi import Depends, FastAPI, HTTPException, status, Request, Form
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
-from pydantic import BaseModel
+#from pydantic import BaseModel
 from database import users_table
 
 import random
