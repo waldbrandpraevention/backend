e2551141e1da06772ea6a4ec1be3189e27f58a77
NSG22 <noelgoldschmidt@web.de>
2023-03-16 19:24:57 +0100

    final fix for incidents

--- api/dependencies/classes.py
+++ api/dependencies/classes.py
@@ -142,7 +142,7 @@ class EventType(Enum):
 class Incident(BaseModel):
     """ Incident class."""
     id:int | None = None
-    drone_name: int | None = None
+    drone_name: str | None = None
     location: str | None = None
     alarm_type: str | None = None
     notes: str | None = None
